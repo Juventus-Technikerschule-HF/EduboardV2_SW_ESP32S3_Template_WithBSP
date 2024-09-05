b730943e405b12bde3215b06cb79597d71bd5c8f
Martin Burger <mburger82@yahoo.de>
2024-09-05 13:43:59 +0200

    add useful comments

--- src/main.c
+++ src/main.c
@@ -18,11 +18,15 @@
 #define UPDATETIME_MS 100
 
 void templateTask(void* param) {
+    //Init stuff here
+
     for(;;) {
+        // task main loop
         if(button_get_state(SW0, true) == SHORT_PRESSED) {
             led_toggle(LED0);
         }
         led_toggle(LED7);
+        // delay
         vTaskDelay(UPDATETIME_MS/portTICK_PERIOD_MS);
     }
 }
@@ -33,7 +37,12 @@ void app_main()
     eduboard2_init();
     
     //Create templateTask
-    xTaskCreate(templateTask, "testTask", 2*2048, NULL, 10, NULL);
+    xTaskCreate(templateTask,   //Subroutine
+                "testTask",     //Name
+                2*2048,         //Stacksize
+                NULL,           //Parameters
+                10,             //Priority
+                NULL);          //Taskhandle
     for(;;) {
         vTaskDelay(2000/portTICK_PERIOD_MS);
         ESP_LOGI(TAG, "Hello Eduboard");
